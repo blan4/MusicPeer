73fbe31d6fa909f063c060738f7fa16c2016b6ca
Ilya Siganov <ilya.blan4@gmail.com>
2016-02-04 11:58:05 +0600

    add music adding flow

--- index.js
+++ index.js
@@ -12,11 +12,25 @@ const logger = require('winston');
 const passport = require('passport');
 const VkStrategy = require('passport-vkontakte').Strategy;
 const jade = require('jade');
+const storage = require('./storage');
 
 function findAudio(accessToken, query) {
   let url = encodeURI(`https://api.vk.com/method/audio.search?auto_complete=1&sort=2&q=${query}&access_token=${accessToken}`);
   logger.info(`Find audio: ${url}`);
-  return request.getAsync(url).then(data => JSON.parse(data.body));
+  return request.getAsync(url).then(data => {
+    const body = JSON.parse(data.body);
+    logger.info(body);
+    return body.response.map(audio => {
+      if (typeof(audio) === 'object' && 'url' in audio) {
+        return {
+          url: audio.url,
+          artist: audio.artist,
+          title: audio.title,
+          duration: audio.duration
+        }
+      }
+    }).filter(el => !!el);
+  });
 }
 
 const app = express();
@@ -45,16 +59,17 @@ passport.use(new VkStrategy({
   version: '5.44'
 }, (accessToken, refreshToken, profile, done) => {
   const user = {
-    userID: profile.id,
+    strategy: 'vkontakte',
+    remoteID: profile.id,
     username: profile.username,
     name: profile.displayName,
     photoURL: profile.photos[0].value,
     profileURL: profile.profileUrl,
     accessToken: accessToken
   };
-  logger.info(`New user: ${user}`);
-  users[user.userID] = user;
-  return done(null, user);
+  return storage.createUser(user).then(user => {
+    return done(null, user);
+  });
 }));
 
 passport.serializeUser(function(user, done) {
@@ -73,8 +88,8 @@ app.get('/audio', (req, res) => {
   const user = req.user;
   const q = req.query.q || 'Нейромонах Феофан';
   if (user) {
-    findAudio(user.accessToken, q).then(data => {
-      res.redirect(data.response[1].url);
+    findAudio(user.accessToken, q).then(audios => {
+      res.send(audios);
     }).catch(err => {
       logger.error(err);
       res.status(400).send(err);
@@ -88,6 +103,89 @@ app.get('/auth/vk', passport.authenticate('vkontakte', {scope: ['audio', 'offlin
 
 app.get('/auth/vk/callback', passport.authenticate('vkontakte', {failureRedirect: '/auth'}), (req, res) => {
   res.redirect('/');
+});
+
+//POST: /room - create new room with random id
+app.post('/room', (req, res) => {
+  storage.createRoom(req.user).then(room => {
+    res.redirect(`/room/${room.id}`);
+  }).catch(err => {
+    logger.error(err);
+    res.status(400).send(err.toString());
+  });
+});
+
+//GET: /rooms - return list of all visited rooms
+app.get('/rooms', (req, res) => {
+
+});
+
+//GET: /room - connect to the room and see current track
+app.get('/room/:id', (req, res) => {
+  const id = req.params.id;
+  storage.findRoom(id).then(room => {
+    res.render('room', {room: room});
+  }).catch(err => {
+    logger.error(err);
+    res.status(404).send(err.toString());
+  });
+});
+
+//POST: /room/:id/next - start next track and get it info
+app.post('/room/:id/next', (req, res) => {
+  const user = req.user;
+  const roomID = req.params.id;
+  if (!user) res.redirect(`/room/${roomID}`);
+  storage.nextTrack(user, roomID).then(track => {
+    res.send(track);
+  }).catch(err => {
+    logger.error(err);
+    res.status(404).send(err.toString());
+  });
+});
+
+app.get('/room/:id/current', (req, res) => {
+  const user = req.user;
+  const roomID = req.params.id;
+  if (!user) res.redirect(`/room/${roomID}`);
+  storage.currentTrack(user, roomID).then(track => {
+    res.send(track);
+  }).catch(err => {
+    logger.error(err);
+    res.status(404).send(err.toString());
+  });
+});
+
+//POST: /room/:id/audio/:aid - add track to room by vk audio id
+app.post('/room/:id/audio', (req, res) => {
+  const id = req.params.id;
+  if (!id) res.redirect('/');
+  const user = req.user;
+  if (!user) res.redirect(`/room/${id}`);
+  const audio = req.body;
+  if (!audio || !audio.url || !audio.title || !audio.artist) res.status(400).send(`Form error ${audio}`);
+  storage.addTrack(id, audio).then(() => {
+    res.send('Success!!');
+  }).catch(err => {
+    logger.error(err);
+    res.status(400).send(err.toString());
+  });
+});
+
+app.get('/room/:id/audio', (req, res) => {
+  const id = req.params.id;
+  if (!id) res.redirect('/');
+  const user = req.user;
+  if (!user) res.redirect(`/room/${id}`);
+  const q = req.query.q || 'Нейромонах Феофан';
+  storage.findRoom(id).then(room => {
+    return findAudio(user.accessToken, q).then(audios => {
+      res.render('addAudio', {room: room, audios: audios});
+    });
+  }).catch(err => {
+    logger.error(err);
+    res.status(400).send(err.toString());
+  })
 });
 
 app.listen(app.get('port'), () => {
---
+++ storage.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const logger = require('winston');
+const PromiseA = require('bluebird');
+const uuid = require('node-uuid');
+
+const roomsRepository = {};
+const usersRepository = {};
+const Storage = {};
+
+Storage.createUser = PromiseA.method(function(user) {
+  const userID = uuid.v4();
+  user.id = userID;
+  usersRepository[userID] = user;
+  logger.info(`New user: ${user}`);
+  return user;
+});
+
+Storage.createRoom = PromiseA.method(function(user) {
+  if (!user || !user.id) throw new Error("User can't be null");
+  const roomID = uuid.v4();
+  const room = {
+    id: roomID,
+    ownerID: user.id,
+    tracks: {
+      current: 0,
+      list: []
+    },
+    updatedAt: new Date(),
+    createdAt: new Date()
+  }
+  roomsRepository[roomID] = room;
+  logger.info(`New room ${room}`);
+  return room;
+});
+
+Storage.findRoom = PromiseA.method(function(id) {
+  if (!id) throw new Error("Room ID can't be null");
+  const room = roomsRepository[id];
+  if (room) return room;
+  throw new Error(`Room with id ${id} not found`);
+});
+
+Storage.addTrack = PromiseA.method(function(roomID, audio) {
+  if (!audio || typeof(audio) != 'object') throw new Error("Audios can't be null");
+  return Storage.findRoom(roomID).then(room => {
+    room.tracks.list.push(audio);
+    logger.info(`Added track to room ${roomID} ${JSON.stringify(audio)}`);
+    return room;
+  });
+});
+
+Storage.nextTrack = PromiseA.method(function(user, roomID) {
+  if (!roomID) throw new Error("Room ID can't be null");
+  if (!user) throw new Error("User can't be null");
+  return Storage.findRoom(roomID).then(room => {
+    if (room.ownerID != user.id) throw new Error(`User ${user.id} can't control Room ${room.id}`);
+    room.tracks.current++;
+    const track = room.tracks.list[room.tracks.current];
+    if (track) return track;
+    throw new Error(`Nothing to play next in room ${roomID}`);
+  });
+});
+
+Storage.currentTrack = PromiseA.method(function(user, roomID) {
+  if (!roomID) throw new Error("Room ID can't be null");
+  if (!user) throw new Error("User can't be null");
+  return Storage.findRoom(roomID).then(room => {
+    if (room.ownerID != user.id) throw new Error(`User ${user.id} can't control Room ${room.id}`);
+    const track = room.tracks.list[room.tracks.current];
+    if (track) return track;
+    throw new Error(`Nothing to play in room ${roomID}`);
+  });
+});
+
+module.exports = Storage;
