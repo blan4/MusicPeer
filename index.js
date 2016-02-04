962624386ea7056fedc7f22f5fe5e51a7c3a5b80
Ilya Siganov <ilya.blan4@gmail.com>
2016-02-04 13:54:43 +0600

    fix vk audio requests

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ function findAudio(accessToken, query) {
     return body.response.map(audio => {
       if (typeof(audio) === 'object' && 'url' in audio) {
         return {
+          id: `${audio.owner_id}_${audio.aid}`,
           url: audio.url,
           artist: audio.artist,
           title: audio.title,
@@ -30,6 +31,23 @@ function findAudio(accessToken, query) {
         }
       }
     }).filter(el => !!el);
+  });
+}
+
+function findAudioById(accessToken, id) {
+  let url = encodeURI(`https://api.vk.com/method/audio.getById?audios=${id}&access_token=${accessToken}`);
+  logger.info(`Find audio: ${url}`);
+  return request.getAsync(url).then(data => {
+    logger.info(`Search audio by id: '${id}'`);
+    const body = JSON.parse(data.body);
+    const audio = body.response[0];
+    return {
+      id: `${audio.owner_id}_${audio.aid}`,
+      url: audio.url,
+      artist: audio.artist,
+      title: audio.title,
+      duration: audio.duration
+    }
   });
 }
 
@@ -126,7 +144,9 @@ app.post('/room/:id/next', (req, res) => {
   const user = req.user;
   const roomID = req.params.id;
   if (!user) res.redirect(`/room/${roomID}`);
-  storage.nextTrack(user, roomID).then(track => {
+  storage.nextTrack(user, roomID).then(audio => {
+    return findAudioById(user.accessToken, audio.id);
+  }).then(track => {
     res.send(track);
   }).catch(err => {
     logger.error(err);
@@ -138,7 +158,9 @@ app.get('/room/:id/current', (req, res) => {
   const user = req.user;
   const roomID = req.params.id;
   if (!user) res.redirect(`/room/${roomID}`);
-  storage.currentTrack(user, roomID).then(track => {
+  storage.currentTrack(user, roomID).then(audio => {
+    return findAudioById(user.accessToken, audio.id);
+  }).then(track => {
     res.send(track);
   }).catch(err => {
     logger.error(err);
@@ -153,7 +175,7 @@ app.post('/room/:id/audio', (req, res) => {
   const user = req.user;
   if (!user) res.redirect(`/room/${id}`);
   const audio = req.body;
-  if (!audio || !audio.url || !audio.title || !audio.artist) res.status(400).send(`Form error ${audio}`);
+  if (!audio || !audio.id) res.status(400).send(`Form error ${audio}`);
   storage.addTrack(id, audio).then(() => {
     res.redirect(`/room/${id}`);
   }).catch(err => {
